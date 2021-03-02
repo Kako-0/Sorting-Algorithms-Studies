2666701b04319fc0fd4c3bc9ae4bc786a1f85fcb
Kayro França <k4yro01@gmail.com>
2021-03-02 20:29:04 -0300

    class WithInsertion usable

--- src/app/App.java
+++ src/app/App.java
@@ -6,6 +6,10 @@ package src.app;
 
 import java.util.Scanner;
 
+import src.Merges.Standard;
+import src.Merges.WithInsertion;
+import src.random.RandomV;
+
 /*Classe principal para iniciar o programa*/
 
 public class App {
@@ -13,31 +17,60 @@ public class App {
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 		int option;
-        
+        long inicio, fim;
+        int a[] = RandomV.randomVet(10000);
+        int res[];
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+",");
+        }
+        System.out.print("\n");
         do{
         	Menu.menu();
             option = in.nextInt();
             switch(option){
             case 1:
-            	System.out.print("Padrão");
+            	System.out.print("Padrão\n");
+
+                inicio = System.currentTimeMillis();
+                res = Standard.mergeSort(a);
+            	fim = System.currentTimeMillis();
+    			System.out.printf("\nTempo de processamento InsertionSort: %.3f ms%n\n", (fim - inicio) / 1000d);
+
+                for (int i = 0; i < res.length; i++) {
+            	    System.out.print(res[i]+",");
+                }
+                
+                System.out.print("\n\n\n\n\n\n");
+
                 break;
             case 2:
-                System.out.print("Usando o InsertionSort para subarrays pequenos");
+                System.out.print("Usando o InsertionSort para subarrays pequenos\n");
+                
+                inicio = System.currentTimeMillis();
+                res = WithInsertion.mergeSort(a);
+            	fim = System.currentTimeMillis();
+    			System.out.printf("\nTempo de processamento InsertionSort: %.3f ms%n\n", (fim - inicio) / 1000d);
+            	
+                for (int i = 0; i < res.length; i++) {
+            	    System.out.print(res[i]+",");
+                }
+                System.out.print("\n\n\n\n\n\n");
+                option=0;
                 break;
             case 3:
-                System.out.print("Testando se o vetor já está ordenado");
+                System.out.print("Testando se o vetor já está ordenado\n");
                 break;
             case 4:
-                System.out.print("Eliminando a cópia para o vetor auxiliar");
+                System.out.print("Eliminando a cópia para o vetor auxiliar\n");
                 break;
             case 0:
-            	System.out.println("Programa encerrado");
+            	System.out.println("Programa encerrado\n");
                 break;
             default:
-                System.out.println("Opção inválida.");
+                System.out.println("Opção inválida.\n");
             }
         } while(option != 0);
-       
+        in.close();
     }
 
 }
