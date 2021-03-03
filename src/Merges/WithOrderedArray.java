32b125ebb31711767153601446cc9fc7f8f875ab
Kayro França <k4yro01@gmail.com>
2021-03-03 20:38:24 -0300

    new class

---
+++ src/Merges/WithOrderedArray.java
@@ -0,0 +1,44 @@
+package src.Merges;
+
+public class WithOrderedArray {
+    public static int[] mergeSort(int[] array) {
+		int[] temp = new int[array.length];
+		return mergeMain(array, temp, 0, array.length - 1);
+	}
+
+	private static int[] mergeMain(int[] array, int[] T, int esq, int dir) {
+		int meio;
+		if (esq < dir) {
+			meio = (esq + dir) / 2;
+			mergeMain(array, T, esq, meio);
+			mergeMain(array, T, meio + 1, dir);
+            if (!(array[meio] <= array[meio + 1])) {
+                merge(array, T, esq, meio + 1, dir);   
+            }
+		}
+		return array;
+	}
+
+	private static void merge(int[] array, int[] T, int esqPos, int dirPos, int dirFim) {
+		int esqFim = dirPos - 1;
+		int tempPos = esqPos;
+		int numElem = dirFim - esqPos + 1;
+
+		while (esqPos <= esqFim && dirPos <= dirFim) {
+			if (array[esqPos] <= array[dirPos]) {
+				T[tempPos++] = array[esqPos++];
+			} else {
+				T[tempPos++] = array[dirPos++];
+			}
+		}
+		while (esqPos <= esqFim)
+			T[tempPos++] = array[esqPos++];
+		while (dirPos <= dirFim)
+			T[tempPos++] = array[dirPos++];
+
+		for (int i = 0; i < numElem; i++) {
+			array[dirFim] = T[dirFim];
+			dirFim--;
+		}
+	}
+}
