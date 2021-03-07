353e6758da13b7913cac6bb951bc30e22e325da6
Kayro França <k4yro01@gmail.com>
2021-03-07 17:15:04 -0300

    update texts

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -5,13 +5,14 @@ package src.app;
 public class Menu {
 	//menu principal
 	public static void menu(){
+        System.out.println("\n-------------------------------------------------------------------\n");
         System.out.println("\tEscolha o método do MergeSort:");
         System.out.println("1. Padrão");
         System.out.println("2. Usando o InsertionSort para subarrays pequenos");
         System.out.println("3. Testando se o vetor já está ordenado");
         System.out.println("4. Eliminando a cópia para o vetor auxiliar");
         System.out.println("0. Sair");
-        System.out.println("Opcao:");
+        System.out.print("Opcao: ");
     }
 	//menu para escolha do numero de elementos
 	public static void menuElementos(){
