867698cb26f148d497708a5dd28df846a9672e37
Kayro França <k4yro01@gmail.com>
2021-03-08 00:17:07 -0300

    fixed bugs

--- src/random/RandomV.java
+++ src/random/RandomV.java
@@ -2,8 +2,8 @@ package src.random;
 
 public class RandomV {
  
-    public static int[] randomVet( int tamanho) {
-        int vetor[] = new int[tamanho];
+    public static Integer[] randomVet( int tamanho) {
+        Integer[] vetor = new Integer[tamanho];
         for (int i = 0; i < vetor.length; i++) {
             vetor[i] = (int)Math.round(Math.random() * tamanho);
         }
