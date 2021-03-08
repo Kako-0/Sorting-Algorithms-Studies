c503adedb644d6d043ed896bf791f454829a5f28
Kayro França <k4yro01@gmail.com>
2021-03-08 00:16:15 -0300

    remove methods about length

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -11,16 +11,8 @@ public class Menu {
         System.out.println("2. Usando o InsertionSort para subarrays pequenos");
         System.out.println("3. Testando se o vetor já está ordenado");
         System.out.println("4. Eliminando a cópia para o vetor auxiliar");
+        System.out.println("5. Todos os MergesSorts");
         System.out.println("0. Sair");
         System.out.print("Opcao: ");
-    }
-	//menu para escolha do numero de elementos
-	public static void menuElementos(){
-        System.out.println("\tEscolha o tamanho do vetor");
-        System.out.println("1. 1.000 elementos");
-        System.out.println("2. 100.000 elementos");
-        System.out.println("3. 1.000.000 elementos");
-        System.out.println("0. Abortar");
-        System.out.println("Opcao:");
     }
 }
