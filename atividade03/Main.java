c12aba484cd26b4fb34f4b8359c4a662c92ff12f
Kayro França <k4yro01@gmail.com>
2021-04-13 16:38:41 -0300

    remove initial codes

--- atividade03/Main.java
+++ atividade03/Main.java
@@ -1,30 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
    public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new FileReader("dados.txt"));
-      ArrayList<String> fifo = new ArrayList<String>();
-      while(br.ready()){ 
-         String linha = br.readLine();
-         String aLinha[] = linha.split(" ");
+      Grafo grafo = new Grafo<>();
 
-         for (String a : aLinha) {
-            fifo.add(a);
-         }
-         fifo.add(" ");
-         
-      }
-      br.close();
-
-      for (String object : fifo) {
-         System.out.print(object + " ");
-         if (object == " ") {
-            System.out.println();
-         }
-      }
+      grafo.getGraph("dados");
+      grafo.showTxt();
 
    }
 }
