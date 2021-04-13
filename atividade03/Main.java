33c6d5c114362dbc074f91cc1340c5ac0a69b42e
Kayro França <k4yro01@gmail.com>
2021-04-12 21:51:55 -0300

    reading the database.txt

--- atividade03/Main.java
+++ atividade03/Main.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
-   public static void main(String[] args) {
-       
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new FileReader("dados.txt"));
+      ArrayList<String> fifo = new ArrayList<String>();
+      while(br.ready()){ 
+         String linha = br.readLine();
+         String aLinha[] = linha.split(" ");
+
+         for (String a : aLinha) {
+            fifo.add(a);
+         }
+         fifo.add(" ");
+         
+      }
+      br.close();
+
+      for (String object : fifo) {
+         System.out.print(object + " ");
+         if (object == " ") {
+            System.out.println();
+         }
+      }
+
    }
 }
